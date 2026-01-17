4cca9c81554cc494411c5007e0b01fa54b059fa4
Kriiif <muhammadfikrifirmansyah0@gmail.com>
2026-01-17 18:52:10 +0700

    feat: Add a new `Meteors` UI component for animated meteor effects.

--- components/ui/meteors.tsx
+++ components/ui/meteors.tsx
@@ -44,15 +44,14 @@ export const Meteors = ({
         <span
           key={"meteor" + idx}
           className={cn(
-            "animate-meteor-effect absolute h-0.5 w-0.5 rotate-[45deg] rounded-[9999px] bg-slate-500 shadow-[0_0_0_1px_#ffffff10]",
+            "absolute h-0.5 w-0.5 rotate-[45deg] rounded-[9999px] bg-slate-500 shadow-[0_0_0_1px_#ffffff10]",
             "before:absolute before:top-1/2 before:h-[1px] before:w-[50px] before:-translate-y-[50%] before:transform before:bg-gradient-to-r before:from-[#64748b] before:to-transparent before:content-['']",
             className,
           )}
           style={{
             top: "0",
             right: meteor.position + "%",
-            animationDelay: meteor.delay + "s",
-            animationDuration: meteor.duration + "s",
+            animation: `meteor ${meteor.duration}s linear ${meteor.delay}s infinite`,
           }}
         />
       ))}
